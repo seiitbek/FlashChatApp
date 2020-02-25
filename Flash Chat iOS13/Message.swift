640c32f7c56adf870eb16655d641e60520d221ea
seiitbek <seiitbek99@gmail.com>
2020-02-25 12:02:57 +0600

    Finished project

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Zhandos on 2/24/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
